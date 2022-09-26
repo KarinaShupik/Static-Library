355425793f7e1de2bcc57fda0f4e9c67501def1d
Karina <105536477+KarinaShupik@users.noreply.github.com>
2022-09-26 10:58:20 +0300

    Add files via upload

---
+++ Myexecrefslib.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Mathfuncslib.h"
+using namespace std;
+int main()
+{
+	double a = 7.4;
+	int b = 99;
+	cout << "a + b = " << Mathfuncs::Mymathfuncs::Add(a, b) << endl;
+	cout << "a - b = " << Mathfuncs::Mymathfuncs::Subtract(a, b) << endl;
+	cout << "a * b = " << Mathfuncs::Mymathfuncs::Multiply(a, b) << endl;
+	cout << "a / b = " << Mathfuncs::Mymathfuncs::Divide(a, b) << endl;
+	return 0;
+}
+
